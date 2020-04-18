53c08299fd43056de6f5e7b3733701013267c87b
Gayathiri Ramamoorthy <rgayathiri2001@yahoo.com>
2020-04-18 17:57:39 -0400

    created prompt questions to build a team

--- app.js
+++ app.js
@@ -33,3 +33,162 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+
+//Employee array
+const Team = []
+
+//Manager specific questions
+const managerQs = [
+    {
+        type: "input",
+        message: "What is your manager's name?",
+        name: "manager",
+         default: "Ram"
+    },
+    {
+        type: "input",
+        message: "What is your manager's ID?",
+        name: "managerId",
+         default: 1
+    },
+    {
+        type: "input",
+        message: "What is your manager's email?",
+        name: "managerEmail",
+         default: "Ram@gmail.com"
+    },
+    {
+        type: "input",
+        message: "What is your manager's office Number",
+        name: "managerOfficeNumber",
+        default: 6132227744
+    },
+
+]
+
+//Engineer specific questions 
+const engineerQs = [{
+    type: "input",
+    message: "What is your engineer's name?",
+    name: "engineer",
+     default: "Tim"
+},
+{
+    type: "input",
+    message: "What is your engineer's ID?",
+    name: "engineerId",
+     default: 2
+},
+{
+    type: "input",
+    message: "What is your engineer's email?",
+    name: "engineerEmail",
+     default: "engineer@m.com"
+},
+{
+    type: "input",
+    message: "What is the engineer's github username?",
+    name: "engineerGithub",
+     default: "tim"
+}
+]
+
+//Intern specific questions
+const interQs = [
+    {
+        type: "input",
+        message: "What is your intern's name?",
+        name: "intern",
+        default: "Adhi"
+    },
+    {
+        type: "input",
+        message: "What is your intern's ID?",
+        name: "internId",
+        default: 10
+    },
+    {
+        type: "input",
+        message: "What is your intern's email?",
+        name: "internEmail",
+        default: "intern@m.com"
+    },
+    {
+        type: "input",
+        message: "What is the intern's school?",
+        name: "internSchool",
+        default: "carleton"
+    },
+]
+
+//Prompt manager questions and then a list question
+function managerQuestions() {
+    inquirer
+        .prompt(managerQs)
+        .then(function (mngrAnswrs) {
+            const addManager = new Manager(mngrAnswrs.manager, mngrAnswrs.managerId, mngrAnswrs.managerEmail, mngrAnswrs.managerOfficeNumber)
+            Team.push(addManager)
+            addteamQs()
+        })
+}
+
+//Prompts engineer specific questions, creates a new engineer and pushes to Team array, also prompts addteamQs
+function addEngineerQs() {
+    inquirer
+        .prompt(engineerQs)
+        .then(function (engineerAnswrs) {
+            const addEngineer = new Engineer(engineerAnswrs.engineer, engineerAnswrs.engineerId, engineerAnswrs.engineerEmail, engineerAnswrs.engineerGithub)
+            Team.push(addEngineer)
+            addteamQs()
+        })
+}
+
+//Prompts intern specific questions, creates a new intern and pushes to Team array, also prompts addteamQs
+function addInternQs() {
+    inquirer
+        .prompt(interQs)
+        .then(function (internAnswrs) {
+            const addIntern = new Intern(internAnswrs.intern, internAnswrs.internId, internAnswrs.internEmail, internAnswrs.internSchool)
+            Team.push(addIntern)
+            addteamQs()
+        })
+}
+
+//List question prompts specific array when a certain choice is picked
+function addteamQs() {
+    inquirer
+        .prompt([{
+            type: "list",
+            message: "What type of team member would you like to add ?",
+            name: "teamMember",
+            choices: ["Engineer", "Intern", "Done"]
+        }])
+        .then(function (answers) {
+            const teamMember = answers.teamMember
+
+            // Calls engineer questions array when engineer is selected
+            if (teamMember === "Engineer") {
+                addEngineerQs()
+
+                //Calls intern array when intern is selected
+            } else if (teamMember === "Intern") {
+                addInternQs()
+
+                //Writes the file when done is selected
+            } else {
+                const renderedOutput = render(Team)
+                fs.writeFile(outputPath, (renderedOutput), function (err) {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log('Team was generated')
+                    }
+                })
+            }
+        })
+}
+
+
+//Prompts manager question to start app
+managerQuestions()
